e07e8756030405167da1bf9ce817ea18dbb17832
Mitchell Lawson-Burton <MitchellBurton@users.noreply.github.com>
2025-04-26 16:57:02 +1200

    Setup fake DB for widgets.

--- src/controllers/widgetController.ts
+++ src/controllers/widgetController.ts
@@ -1,21 +1,39 @@
-import { Controller, Get, Path, Route } from "tsoa";
+import { UUID } from "../lib/types";
+import { findById } from "../repositories/widgetRepository";
+import { Controller, Get, Path, Res, Route, TsoaResponse } from "tsoa";
 
+/**
+ * Represents a widget with its associated properties.
+ */
 interface Widget {
-  id: string;
+  /** The unique identifier for the widget. */
+  id: UUID;
+  /** The name of the widget. */
   name: string;
+  /** A brief description of the widget. */
   description: string;
+  /** A URL for the widget's image. */
   image: string;
 }
 
 @Route("widgets")
 export class WidgetsController extends Controller {
+  /**
+   * Retrieves a widget by its unique identifier.
+   * @param widgetId The unique identifier of the widget to retrieve.
+   */
   @Get("{widgetId}")
-  public async getWidget(@Path() widgetId: string): Promise<Widget> {
-    return {
-      id: widgetId,
-      name: "widget",
-      description: "A widget",
-      image: "https://an.image.url",
-    };
+  public async getWidget(
+    @Path() widgetId: UUID,
+    @Res() notFoundResponse: TsoaResponse<404, { reason: string }>
+  ): Promise<Widget> {
+    const widget = findById(widgetId);
+    if (widget) {
+      return widget;
+    }
+
+    // Widget is not found, 404.
+    return notFoundResponse(404, { reason: "Widget not found" });
   }
+  // TODO: Implement the create, list, and update methods
 }
---
+++ src/repositories/widgetRepository.ts
@@ -0,0 +1,90 @@
+import { UUID } from "../lib/types";
+import { v4 as uuidv4 } from "uuid";
+
+interface WidgetDBModel {
+  id: UUID;
+  name: string;
+  description: string;
+  image: string;
+}
+
+interface WidgetDB {
+  [key: string]: WidgetDBModel;
+}
+
+const widgetDB: WidgetDB = {
+  "8e7dd84a-066d-48f8-9ea8-cab94e50556d": {
+    id: "8e7dd84a-066d-48f8-9ea8-cab94e50556d",
+    name: "widget",
+    description: "A widget",
+    image: "https://an.image.url",
+  },
+  "d97db27a-71cc-4022-abf1-01ebf9033caa": {
+    id: "d97db27a-71cc-4022-abf1-01ebf9033caa",
+    name: "widget2",
+    description: "A widget2",
+    image: "https://an.image.url",
+  },
+};
+
+/**
+ * Retrieves a widget by its unique identifier.
+ *
+ * @param id - The unique identifier of the widget to retrieve.
+ * @returns The widget object if found, otherwise `null`.
+ */
+export const findById = (id: string) => {
+  const widget = widgetDB[id];
+  if (widget) {
+    return widget;
+  }
+  return null;
+};
+
+/**
+ * Creates a new widget and assigns it a unique identifier.
+ *
+ * @param widget - An object representing the widget to be created, excluding the `id` property.
+ * @returns The newly created widget, including its generated `id`.
+ */
+export const create = (widget: Omit<WidgetDBModel, "id">): WidgetDBModel => {
+  // Simulate saving to a database by adding it to the in-memory object.
+  // We are using a Guid for the id, so can assume no conflicts.
+  // The the real world, we would let the DB handle it.
+  const newWidget: WidgetDBModel = { ...widget, id: uuidv4() };
+  widgetDB[newWidget.id] = newWidget;
+  return newWidget;
+};
+
+/**
+ * Updates an existing widget in the database with the provided partial data.
+ *
+ * @param id - The unique identifier of the widget to update.
+ * @param widget - A partial object containing the properties to update in the widget.
+ * @returns The updated widget object if the widget exists, or `null` if no widget with the given ID is found.
+ */
+export const update = (id: string, widget: Partial<WidgetDBModel>) => {
+  const existingWidget = widgetDB[id];
+  if (existingWidget) {
+    const updatedWidget = { ...existingWidget, ...widget };
+    widgetDB[id] = updatedWidget;
+    return updatedWidget;
+  }
+  return null;
+};
+
+/**
+ * Removes a widget from the widget database by its ID.
+ *
+ * @param id - The unique identifier of the widget to be removed.
+ * @returns A boolean indicating whether the widget was successfully removed.
+ *          Returns `true` if the widget existed and was removed, otherwise `false`.
+ */
+export const remove = (id: string) => {
+  const widget = widgetDB[id];
+  if (widget) {
+    delete widgetDB[id];
+    return true;
+  }
+  return false;
+};
